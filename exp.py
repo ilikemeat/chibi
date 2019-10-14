ee449af1001bfc6d7ba2f7ecc07344ad43d2bb5e
ITO Ayari <21716011@jwu.ac.jp>
2019-10-14 16:23:52 +0900

    exp3

--- exp.py
+++ exp.py
@@ -1,7 +1,8 @@
 
+class Expr(object):
+    pass
 
-
-class Val(object):
+class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
@@ -14,7 +15,12 @@ v = Val(1)
 print(v)
 assert v .eval() == 1
 
-class Add(object):
+
+assert isinstance(v,Expr)
+assert isinstance(v,Val)
+assert isinstance(v,int)
+
+class Add(Expr):
     __slots__=['left','right']
     def __init__(self,a,b):
         self.left = a
@@ -25,7 +31,8 @@ class Add(object):
 e = Add(Val(1),Val(2))
 assert e .eval() == 3
  
-
+e = Add(1,2)
+assert e.eval() == 3
 
 e = Add(Val(1),Add(Val(2),Val(3)))
 assert e. eval() == 6
