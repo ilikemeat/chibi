f3b6ba396c6e37ecb7571c64ebc2d228ca40b5d6
ITO Ayari <21716011@jwu.ac.jp>
2019-10-28 16:48:40 +0900

    parse3

--- parse.py
+++ parse.py
@@ -1,20 +1,24 @@
-from exp import Val,Add
+from exp import Val,Add,Sub,Mul,Div
 
 
 
 def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        num = int(s)
-        return Val(num)
-    else:
+    
+    if s.find('+') >0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1),parse(s2))
+    if s.find('*') >0:
+        pos = s.find('+')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1),parse(s2))
+    return Val(int(s))
 
-
-e = parse("1+2+3")
+e = parse("1-2-3")
 print(e,e.eval())
+assert e.eval() == -4
 
 
 
