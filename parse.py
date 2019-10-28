3f5e3a135c2025063da9b4508e5c309e87aa2b03
ITO Ayari <21716011@jwu.ac.jp>
2019-10-28 16:23:20 +0900

    parse2

--- parse.py
+++ parse.py
@@ -1,18 +1,22 @@
 from exp import Val,Add
 
+
+
 def parse(s: str):
-  num = int(s)
-  return Val(num)
+    pos = s.find('+')
+    if pos == -1:
+        num = int(s)
+        return Val(num)
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1),parse(s2))
 
-e = parse("123")
-print(e)
 
-s = "123+456"
-pos = s.find('+')
-print('pos',pos)
+e = parse("1+2+3")
+print(e,e.eval())
 
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s,s1,s2)
+
+
 
 
